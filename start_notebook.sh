9a6ac4362bd6965a9575de414b90a4b8fafc41cb
Andreas Backhaus <aback@gmx.de>
2020-05-01 13:31:17 +0200

    nvidia dali pipeline works now

--- start_notebook.sh
+++ start_notebook.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 # Generate a virtual environment with notebooks name
-python3 -m venv $1
+python3.7 -m venv $1
 # Activate environment
 source $1/bin/activate
 # Install iPython
-pip install wheel
-pip install ipykernel
-pip install jupyter
+pip3 install --upgrade pip
+pip3 install wheel
+pip3 install ipykernel
+pip3 install jupyter
 # create kernel
 ipython kernel install --user --name=$1
 # start jupyter notebook - you have to choose kernel by hand so far!
